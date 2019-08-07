583243d048363ea6e710764f98b2c29b6e22f96a
d10xa <d10xa@mail.ru>
2019-08-08 00:26:22 +0300

    Refactoring

--- src/main/scala/ru/d10xa/file_adventure/Main.scala
+++ src/main/scala/ru/d10xa/file_adventure/Main.scala
@@ -6,17 +6,15 @@ import better.files._
 import me.tongfei.progressbar.ProgressBar
 import org.apache.commons.codec.digest.DigestUtils.sha256Hex
 
-
 object Main {
 
-  def filePredicate(f: File): Boolean = {
+  def filePredicate(f: File): Boolean =
     Seq(
       !f.isHidden,
       !f.name.startsWith("."),
       !f.name.endsWith(".sfv"),
       f.isRegularFile
     ).forall(identity)
-  }
 
   def fileToHash(file: File, bar: ProgressBar): String = {
     val _ = bar.setExtraMessage(file.name)
@@ -25,25 +23,30 @@ object Main {
     sum
   }
 
-  val filesToHashes: Vector[File] => Vector[String] = files => (
-    for {
-      bar <- new ProgressBar("", files.map(_.size).sum).autoClosed
-    } yield files.map(fileToHash(_, bar))
-    ).get()
+  val filesToHashes: Vector[File] => Vector[String] = files =>
+    new ProgressBar("", files.map(_.size).sum).autoClosed
+      .map(bar => files.map(fileToHash(_, bar)))
+      .get()
 
-  val sortHashes: Vector[String] => Vector[String] = _.sortBy(new BigInteger(_, 16))
+  val sortHashes: Vector[String] => Vector[String] =
+    _.sortBy(new BigInteger(_, 16))
 
-  val toSingleHash: Vector[String] => String = {
+  val sortedHashesToSingleHash: Vector[String] => String = {
     case Vector(x: String) => x
     case xs => sha256Hex(xs.mkString(""))
   }
+
+  val filesToSingleHash: Vector[File] => String =
+    sortedHashesToSingleHash
+      .compose(sortHashes)
+      .compose(filesToHashes)
 
   def main(args: Array[String]): Unit = {
     val files = File(args.head)
       .list(filePredicate)
       .toList
 
-    val hash: String = (toSingleHash compose sortHashes compose filesToHashes) (files.toVector)
+    val hash: String = filesToSingleHash(files.toVector)
 
     println(hash)
   }
