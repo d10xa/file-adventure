84f59f81b10776b32ed25d56e0427a7847d7ba7d
d10xa <d10xa@mail.ru>
2020-09-18 16:11:34 +0300

    Add check command tests

---
+++ src/test/scala/ru/d10xa/file_adventure/CheckTest.scala
@@ -0,0 +1,25 @@
+package ru.d10xa.file_adventure
+
+import better.files.File
+import cats.effect.IO
+
+class CheckTest extends TestBase {
+
+  test("check ok") {
+    val checkedFiles = checkDir(File("src/test/file/check/ok"))
+    checkedFiles.size shouldBe 1
+    val file = checkedFiles.head
+    file.valid shouldBe true
+  }
+
+  test("check fail") {
+    val checkedFiles = checkDir(File("src/test/file/check/fail"))
+    checkedFiles.size shouldBe 1
+    val file = checkedFiles.head
+    file.valid shouldBe false
+  }
+
+  def checkDir(d: File): List[CheckedFile] =
+    new Check[IO].checkDir(d).unsafeRunSync()
+
+}
