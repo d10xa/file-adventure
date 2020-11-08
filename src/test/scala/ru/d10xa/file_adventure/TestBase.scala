bae77defedfb75302c7711a7be6ac6ec3b174842
d10xa <d10xa@mail.ru>
2020-11-09 00:09:03 +0300

    Add ContextShift and change progressBuilder in TestBase

--- src/test/scala/ru/d10xa/file_adventure/TestBase.scala
+++ src/test/scala/ru/d10xa/file_adventure/TestBase.scala
@@ -1,7 +1,6 @@
 package ru.d10xa.file_adventure
 
 import cats.effect.IO
-import cats.effect.Resource
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 import ru.d10xa.file_adventure.progress.Progress.ProgressBuilder
@@ -9,24 +8,18 @@ import ru.d10xa.file_adventure.fs.Checksum
 import ru.d10xa.file_adventure.fs.Fs
 import ru.d10xa.file_adventure.progress.Progress
 import ru.d10xa.file_adventure.progress.TraverseProgress
+import scala.concurrent.ExecutionContext
 
 abstract class TestBase extends AnyFunSuite with Matchers {
+  implicit val ec = IO.contextShift(ExecutionContext.global)
   implicit val checksum: Checksum[IO] =
     Checksum.make[IO].unsafeRunSync()
   implicit val console: Console[IO] =
     Console.make[IO].unsafeRunSync()
   implicit val log: Log[IO] =
     Log.make[IO](debug = true)
-  implicit val progressBuilder: ProgressBuilder[IO] = new ProgressBuilder[IO] {
-    override def build(
-      params: Progress.InitParams
-    ): Resource[IO, Progress[IO]] =
-      Resource.pure(new Progress[IO] {
-        override def setExtraMessage(msg: String): IO[Unit] = IO.unit
-
-        override def stepBy(n: Long): IO[Unit] = IO.unit
-      })
-  }
+  implicit val progressBuilder: ProgressBuilder[IO] =
+    Progress.dummyBuilder[IO].unsafeRunSync()
   implicit val traverseProgress: TraverseProgress[IO] =
     TraverseProgress.make[IO](progressBuilder)
   implicit val fs: Fs[IO] = Fs.make[IO].unsafeRunSync()
